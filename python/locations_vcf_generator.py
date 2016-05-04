b8e7786f0cd45981689d789933f0a2f3ecca573b
floppyears <cedenoj@onid.oregonstate.edu>
2016-05-03 19:29:12 -0700

    Increase page size
    CO-417

--- python/locations_vcf_generator.py
+++ python/locations_vcf_generator.py
@@ -138,7 +138,7 @@ access_token_response = getAccessToken(access_token_url, config_data["client_id"
 
 access_token  = access_token_response["access_token"]
 locations_url = base_url + config_data["locations_endpoint"]
-params        = {"type": "dining", "page[size]":100}
+params        = {"type": "dining", "page[size]":200}
 
 # Process Dining Data
 response = getLocations(locations_url, access_token, params)
