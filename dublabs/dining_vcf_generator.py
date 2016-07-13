a52b782db940964cdb85ee87ea859048cd2f3e8c
floppyears <cedenoj@onid.oregonstate.edu>
2016-07-13 16:03:40 -0700

    Refactoring

--- dublabs/dining_vcf_generator.py
+++ dublabs/dining_vcf_generator.py
@@ -1,6 +1,7 @@
 from HTMLParser import HTMLParser
 from datetime import datetime
 from operator import itemgetter
+from dublabs import api 
 
 import json
 import pycurl
@@ -14,47 +15,6 @@ CAMPUS = "Oregon State - Corvallis"
 CAMPUS_LOC = "C"
 
 DEBUG = False
-
-def getAccessToken(url, client_id, client_secret):
-    post_data = "client_id=" + client_id + "&client_secret=" + client_secret + "&grant_type=client_credentials"
-
-    storage = StringIO.StringIO()
-    curl    = pycurl.Curl()
-
-    # Set options
-    curl.setopt(pycurl.URL, url)                     # CURLOPT_URL in PHP
-    curl.setopt(pycurl.POST, 1)                      # CURLOPT_POST in PHP
-    curl.setopt(pycurl.POSTFIELDS, post_data)        # CURLOPT_POSTFIELDS in PHP
-    curl.setopt(pycurl.WRITEFUNCTION, storage.write) # CURLOPT_RETURNTRANSFER in PHP
-
-    # Send the request and save response
-    curl.perform()
-
-    # Close request to clear up some resources
-    curl.close()
-
-    response = storage.getvalue()
-    return json.loads(response)
-
-
-def getLocations(url, access_token, params):
-    query_params = urllib.urlencode(params)
-    api_call_url = url + "?" + query_params;
-    headers      = ['Authorization: Bearer '+ access_token]
-
-    storage = StringIO.StringIO()
-    curl    = pycurl.Curl()
-
-    # Set options
-    curl.setopt(pycurl.URL, api_call_url)
-    curl.setopt(pycurl.HTTPHEADER, headers) # CURLOPT_HTTPHEADER in PHP
-    curl.setopt(pycurl.WRITEFUNCTION, storage.write)
-
-    curl.perform()
-    curl.close()
-
-    response = storage.getvalue()
-    return json.loads(response)
 
 def getCampus():
     entry = vobject.vCard()
@@ -273,14 +233,14 @@ config_data      = json.load(config_data_file)
 base_url         = config_data["hostname"] + config_data["version"] + config_data["api"]
 access_token_url = base_url + config_data["token_endpoint"]
 
-access_token_response = getAccessToken(access_token_url, config_data["client_id"], config_data["client_secret"]);
+access_token_response = api.getAccessToken(access_token_url, config_data["client_id"], config_data["client_secret"]);
 
 access_token  = access_token_response["access_token"]
 locations_url = base_url + config_data["locations_endpoint"]
 params        = {"type": "dining", "page[size]":200}
 
 # Process Dining Data
-response = getLocations(locations_url, access_token, params)
+response = api.getLocations(locations_url, access_token, params)
 writeVcardFile('dininglocations.vcf', response)
 
 #@todo: sed -i='' 's/\\;/;/g' dininglocations.vcf && sed -i='' 's/\\,/,/g' dininglocations.vcf
